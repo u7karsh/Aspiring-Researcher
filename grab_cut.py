9c877b527bb106f8ee73ef17dbd18f8398c8bb82
u7karsh <u7karsh@yahoo.co.in>
2015-03-09 02:00:11 +0530

    auto bounding box based on % margin

--- grab_cut.py
+++ grab_cut.py
@@ -1,18 +1,32 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('messi5.jpg')
-mask = np.zeros(img.shape[:2],np.uint8)
+##height and width margin in percentages!
+height_margin = 10.0
+width_margin = 10.0
 
+#read the image
+img = cv2.imread('messi5.jpg')
+
+#compute the size of image
+height, width = img.shape[:2]
+
+##mask for grab cut algo
+mask = np.zeros((height, width),np.uint8)
+
+##auto bounding box generation
+rect = (int((width_margin/200)*width),int((height_margin/200)*height),int(width-(width_margin/100)*width-1),int(height-(height_margin/100)*height-1))
+
+###grab cut algorithm
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
-
-rect = (50,50,450,290)
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+
+##mask out the image
 img = img*mask2[:,:,np.newaxis]
 
+##display the output
 cv2.imshow("out", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
